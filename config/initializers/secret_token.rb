722b22d39fd0602a937102de4108f478c95208ba
Dejan Bundalo <dejan@db-laptop.(none)>
2011-12-10 13:16:57 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webtoday::Application.config.secret_token = '23a74da0aee877e6124b72d3afd2323c52786803ad1bbb2365df3974d960820bf0884efc0c1f47abe537d0146ea9f96314656e4a28aef9a19677a3a647464d4c'
